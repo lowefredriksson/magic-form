53e8dc385863c566420fba0d5b835c78959dbcbb
lowefredriksson <lowe@barkh.se>
2020-08-03 23:50:58 +0200

    s

--- src/lowely.tsx
+++ src/lowely.tsx
@@ -18,36 +18,32 @@ export const Child: React.FC<{
   const { setValue, getValue, registerField } = useContext(Context);
   useEffect(() => {
     registerField(null, name, {
-      validate: (value: Value) => {
+      validate: (value: Value, values: Map<string, Value>) => {
         return value < 0 ? "Should be positive" : undefined;
       },
     });
-  }, [registerField]);
+  }, [registerField, name]);
   const wVal = useValue(name);
   const counter = useRenderCounter();
+  const increase = () => {
+    const prev = getValue(name);
+    setValue(name, ((prev as number) ?? 0) - 1);
+  };
+  const decrease = () => {
+    const prev = getValue(name);
+    setValue(name, ((prev as number) ?? 0) + 1);
+  };
   return (
     <>
       <div>
         {name} {counter}
       </div>
       <div style={{ display: "flex", flexDirection: "row" }}>
-        <button
-          type="button"
-          onClick={() => {
-            const prev = getValue(name);
-            setValue(name, ((prev as number) ?? 0) - 1);
-          }}
-        >
+        <button type="button" onClick={increase}>
           Minus
         </button>
         The watch {wVal}
-        <button
-          type="button"
-          onClick={() => {
-            const prev = getValue(name);
-            setValue(name, ((prev as number) ?? 0) + 1);
-          }}
-        >
+        <button type="button" onClick={decrease}>
           Plus
         </button>
       </div>
@@ -55,27 +51,49 @@ export const Child: React.FC<{
   );
 };
 
-const Field = ({ name }: { name: string }) => {
-  const { setValue, registerField } = useContext(Context);
-  const wVal = useValue(name);
-  const counter = useRenderCounter();
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(name, event.target.value);
-  };
-  const config = {
-    validate: (value: any) => {
-      return value.length < 4 ? "Should be atlest 4" : undefined;
+const useField = (name: string, config: FieldConfig) => {
+  const { setValue, setTouched, registerField } = useContext(Context);
+  const onChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(name, event.target.value);
     },
+    [setValue, name]
+  );
+  const onBlur = useCallback(
+    (event: React.FocusEvent<HTMLInputElement>) => {
+      setTouched(name);
+    },
+    [setTouched, name]
+  );
+  const register = useCallback(
+    (ref: HTMLInputElement) => {
+      registerField(ref, name, config);
+    },
+    [name, config, registerField]
+  );
+  return {
+    name,
+    id: `${name}_input`,
+    onChange,
+    onBlur,
+    ref: register,
   };
+};
+
+const Field = ({
+  name,
+  validate,
+}: {
+  name: string;
+  validate?: ValidationResolver;
+}) => {
+  const counter = useRenderCounter();
+  const fieldProps = useField(name, {
+    validate,
+  });
   return (
     <>
-      <p>Value: {wVal}</p>
-      <input
-        ref={(ref) => registerField(ref, name, config)}
-        name={name}
-        id={`${name}_input`}
-        onChange={onChange}
-      />
+      <input {...fieldProps} />
       <p>Renders: {counter}</p>
     </>
   );
@@ -84,6 +102,7 @@ const Field = ({ name }: { name: string }) => {
 const ErrorComponent = ({ name }: { name: string }) => {
   const count = useRenderCounter();
   const error = useError(name);
+  const touched = useTouched(name);
   return (
     <div
       style={{
@@ -94,225 +113,314 @@ const ErrorComponent = ({ name }: { name: string }) => {
       }}
     >
       <p>Renders {count}</p>
-      {error ? <p>{error}</p> : null}
+      {error && touched ? <p>{error}</p> : null}
     </div>
   );
 };
 
 type Error = string | undefined;
 type Value = number | string;
+type ValidationResolver = (
+  value: Value,
+  values: Map<string, Value>
+) => string | undefined;
 type FieldConfig = {
-  validate?: (value: Value) => string | undefined;
+  validate?: ValidationResolver;
 };
 type Field = { ref?: HTMLInputElement | null; config: FieldConfig };
-type Listner = {
+type Listener = {
   id: string;
   callback: (value: Value) => void;
   listenTo: string;
 };
 
+type RegisterListener<T> = (
+  name: string,
+  id: string,
+  callback: (value: T) => void
+) => void;
+
+type UnregisterListener = (id: string) => void;
+
 type ContextType = {
-  setValue: (name: string, value: Value) => void;
-  getValue: (name: string) => Value | undefined;
-  registerValueCallback: (
-    name: string,
-    id: string,
-    callback: (value: Value) => void
-  ) => void;
-  unregisterValueCallback: (id: string) => void;
-  registerErrorCallback: (
-    name: string,
-    id: string,
-    callback: (value: Error) => void
-  ) => void;
-  unregisterErrorCallback: (id: string) => void;
+  registerValueListener: RegisterListener<Value>;
+  unregisterValueListener: UnregisterListener;
+  registerErrorListener: RegisterListener<Error>;
+  unregisterErrorListener: UnregisterListener;
+  registerTouchedListener: RegisterListener<boolean>;
+  unregisterTouchedListener: UnregisterListener;
   registerField: (
     ref: HTMLInputElement | null,
     key: string,
     config: FieldConfig
   ) => void;
   getError: (name: string) => string | undefined;
+  setTouched: (key: string, isTouched?: boolean) => void;
+  getTouched: (key: string) => boolean | undefined;
+  setValue: (key: string, value: Value) => void;
+  getValue: (key: string) => Value | undefined;
 };
 
-const Context = React.createContext<ContextType>({
-  setValue: () => {},
-  getValue: () => undefined,
-  registerValueCallback: () => {},
-  unregisterValueCallback: () => {},
-  registerField: () => {},
-  unregisterErrorCallback: () => {},
-  registerErrorCallback: () => {},
-  getError: () => undefined,
-});
+function useListener<T>(
+  key: string,
+  id: string,
+  register: RegisterListener<T>,
+  unregister: UnregisterListener
+) {
+  const [value, setValue] = useState<T | undefined>(undefined);
+  const idRef = useRef(id);
+  useEffect(() => {
+    register(key, idRef.current, setValue);
+    return () => {
+      unregister(idRef.current);
+    };
+  }, [register, unregister, idRef, setValue, key]);
+  return value;
+}
 
 const useValue = (key: string) => {
-  const { registerValueCallback, unregisterValueCallback } = useContext(
+  const { registerValueListener, unregisterValueListener } = useContext(
     Context
   );
-  const [value, setValue] = useState<Value>(0);
-  const idRef = useRef(`${key}_subscription`);
-  useEffect(() => {
-    registerValueCallback(key, idRef.current, setValue);
-    return () => {
-      unregisterValueCallback(idRef.current);
-    };
-  }, [registerValueCallback, unregisterValueCallback, idRef]);
-  return value;
+  return useListener<Value>(
+    key,
+    `${key}_subscription`,
+    registerValueListener,
+    unregisterValueListener
+  );
 };
 
 const useError = (key: string) => {
-  const { registerErrorCallback, unregisterErrorCallback } = useContext(
+  const { registerErrorListener, unregisterErrorListener } = useContext(
     Context
   );
-  const [error, setError] = useState<Error>(undefined);
-  const idRef = useRef(`${key}_error_subscription`);
-  useEffect(() => {
-    registerErrorCallback(key, idRef.current, setError);
-    return () => {
-      unregisterErrorCallback(idRef.current);
-    };
-  }, [registerErrorCallback, unregisterErrorCallback, idRef]);
-  return error;
+  return useListener<Error>(
+    key,
+    `${key}_error_subscription`,
+    registerErrorListener,
+    unregisterErrorListener
+  );
 };
 
-const useListners = () => {
-  const listners = useRef<Listner[]>([]);
+const useTouched = (key: string) => {
+  const { registerTouchedListener, unregisterTouchedListener } = useContext(
+    Context
+  );
+  return useListener<boolean>(
+    key,
+    `${key}_touched_subscription`,
+    registerTouchedListener,
+    unregisterTouchedListener
+  );
+};
+
+const useListeners = () => {
+  const listeners = useRef<Listener[]>([]);
   const registerCallback = useCallback(
     (listenTo: string, id: string, callback: (value: any) => void) => {
-      listners.current = [
-        ...listners.current.filter(({ id: _id }) => _id !== id),
+      listeners.current = [
+        ...listeners.current.filter(({ id: _id }) => _id !== id),
         { listenTo, id, callback },
       ];
     },
-    [listners]
+    [listeners]
   );
   const unregisterCallback = useCallback(
     (id: string) => {
-      listners.current = listners.current.filter(({ id: _id }) => _id !== id);
+      listeners.current = listeners.current.filter(({ id: _id }) => _id !== id);
     },
-    [listners.current]
+    [listeners.current]
   );
-  return [listners, registerCallback, unregisterCallback] as const;
+  return [listeners, registerCallback, unregisterCallback] as const;
 };
 
-export const Lowely = () => {
-  const values = useRef<{ [key in string]: Value }>({});
-  const fields = useRef<{ [key in string]: Field }>({});
-  const errors = useRef<{ [key in string]: string | undefined }>({});
-  const touched = useRef<{ [key in string]: boolean }>({});
-  const [
-    errorListners,
-    registerErrorCallback,
-    unregisterErrorCallback,
-  ] = useListners();
-  const [
-    valueListners,
-    registerValueCallback,
-    unregisterValueCallback,
-  ] = useListners();
-  const registerField = (
-    ref: HTMLInputElement | null,
-    key: string,
-    config: FieldConfig = {}
-  ) => {
-    fields.current = {
-      ...fields.current,
-      [key]: {
-        ref,
-        config,
-      },
-    };
-  };
+const notifyListeners = (
+  listners: React.MutableRefObject<Listener[]>,
+  name: string,
+  value: any
+) => {
+  listners.current.forEach((listner) => {
+    if (listner.listenTo === name) {
+      listner.callback(value);
+    }
+  });
+};
+
+function useForm({
+  onSubmit,
+}: {
+  onSubmit: (values: Map<string, Value>) => Promise<any>;
+}) {
+  const values = useRef<Map<string, Value>>(new Map());
+  const fields = useRef<Map<string, Field>>(new Map());
+  const errors = useRef<Map<string, Error>>(new Map());
+  const touched = useRef<Set<string>>(new Set());
+  const registerField = useCallback(
+    (ref: HTMLInputElement | null, key: string, config: FieldConfig = {}) => {
+      fields.current.set(key, { ref, config });
+    },
+    [fields]
+  );
   const unregisterField = (key: string) => {
-    // Todo
+    fields.current.delete(key);
+    values.current.delete(key);
+    touched.current.delete(key);
+    errors.current.delete(key);
   };
-  const notifyListners = (
-    listners: React.MutableRefObject<Listner[]>,
-    name: string,
-    value: any
-  ) => {
-    // check if value has changed?
-    listners.current.forEach((listner) => {
-      if (listner.listenTo === name) {
-        listner.callback(value);
-      }
-    });
-  };
-  //   const validateValues = () => {
-  //     const newErrors = Object.keys(fields.current)
-  //       .map((key) => ({ ...fields.current[key], key }))
-  //       .reduce((acc, field) => {
-  //         if (field.config.validate) {
-  //           const error = field.config.validate(values.current[field.key]);
-  //           return {
-  //             ...acc,
-  //             [field.key]: error,
-  //           };
-  //         }
-  //         return acc;
-  //       }, {});
-  //     errors.current = newErrors;
-  //   };
+  const [
+    errorListeners,
+    registerErrorListener,
+    unregisterErrorListener,
+  ] = useListeners();
+  const [
+    valueListeners,
+    registerValueListener,
+    unregisterValueListener,
+  ] = useListeners();
+  const [
+    touchedListeners,
+    registerTouchedListener,
+    unregisterTouchedListener,
+  ] = useListeners();
 
   const validateValue = (key: string, value: Value) => {
-    console.log("validateValue", key, value);
-    const field = fields.current[key];
-    if (field.config.validate) {
-      const prev = errors.current[key];
-      const next = field.config.validate(value);
-      errors.current = {
-        ...errors.current,
-        [key]: field.config.validate(value),
-      };
-      console.log("prev", prev, "next", next);
+    const field = fields.current.get(key);
+    if (field?.config.validate) {
+      const prev = errors.current.get(key);
+      const next = field.config.validate(value, values.current);
+      errors.current.set(key, next);
       if (prev !== next) {
-        notifyListners(errorListners, key, next);
+        notifyListeners(errorListeners, key, next);
       }
     }
   };
+
   const setValue = useCallback(
     (key: string, value: Value) => {
-      values.current = { ...values.current, [key]: value };
-      notifyListners(valueListners, key, value);
-      validateValue(key, value);
+      const prev = values.current.get(key);
+      values.current.set(key, value);
+      if (prev !== value) {
+        notifyListeners(valueListeners, key, value);
+        values.current.forEach((value, key, map) => {
+          validateValue(key, value);
+        });
+      }
     },
-    [values]
+    [values, notifyListeners, valueListeners, validateValue]
   );
-  const getValue = useCallback(
+  const setTouched = useCallback(
     (key: string) => {
-      return values.current[key];
+      if (!touched.current.has(key)) {
+        notifyListeners(touchedListeners, key, true);
+      }
+      touched.current.add(key);
     },
-    [values]
+    [touched, notifyListeners, touchedListeners]
   );
-  const getError = useCallback(
-    (key: string) => {
-      return errors.current[key];
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // set is validating ref true
+    // set all fields touched
+    // run validation
+
+    // : if error :
+
+    // focus first error field
+
+    // : else :
+
+    // set is validating ref false
+    // set is submiting ref true
+    // call user onSumbit
+    // set is submitting ref false
+
+    // : if error :
+    // set submitError ref
+
+    // : else :
+
+    console.log("onSubmit");
+  };
+
+  const getValue = (key: string) => values.current.get(key);
+  const getError = (key: string) => errors.current.get(key);
+  const getTouched = (key: string) => touched.current.has(key);
+  return {
+    registerField,
+    unregisterField,
+    setValue,
+    registerValueListener,
+    unregisterValueListener,
+    registerErrorListener,
+    unregisterErrorListener,
+    setTouched,
+    registerTouchedListener,
+    unregisterTouchedListener,
+    getValue,
+    getError,
+    getTouched,
+    handleSubmit,
+  } as const;
+}
+
+const Context = React.createContext<ContextType>({
+  registerValueListener: () => {},
+  unregisterValueListener: () => {},
+  registerField: () => {},
+  unregisterErrorListener: () => {},
+  registerErrorListener: () => {},
+  unregisterTouchedListener: () => {},
+  registerTouchedListener: () => {},
+  getError: () => undefined,
+  setTouched: () => {},
+  getTouched: () => false,
+  setValue: () => {},
+  getValue: () => undefined,
+});
+
+export const Lowely = () => {
+  const { handleSubmit, ...formBag } = useForm({
+    onSubmit: () => {
+      return Promise.resolve(true);
     },
-    [errors]
-  );
-  const counter = useRef(0);
-  counter.current++;
+  });
+  const count = useRenderCounter();
   return (
-    <Context.Provider
-      value={{
-        setValue,
-        getValue,
-        registerValueCallback,
-        unregisterValueCallback,
-        registerField,
-        registerErrorCallback,
-        unregisterErrorCallback,
-        getError,
-      }}
-    >
+    <Context.Provider value={formBag}>
       <div>
-        <Field name="firstname" />
-        <ErrorComponent name="firstname" />
-        <Child name={"First Child"} />
-        <ErrorComponent name="First Child" />
-        <Child name={"Second Child"} />
-        <ErrorComponent name="Second Child" />
-        Renders {counter.current}
+        <form onSubmit={handleSubmit}>
+          <Field
+            name="firstname"
+            validate={(value: Value) =>
+              (value as string).length < 4
+                ? "Should be at least 4 characters"
+                : undefined
+            }
+          />
+          <ErrorComponent name="firstname" />
+          <Field
+            name="password"
+            validate={(value: Value) =>
+              (value as string).length < 4
+                ? "Should be at least 4 characters"
+                : undefined
+            }
+          />
+          <ErrorComponent name="password" />
+          <Field
+            name="confirm password"
+            validate={(value: Value, values) =>
+              (value as string).length < 4
+                ? "Should be at least 4 characters"
+                : value !== values.get("password")
+                ? "Should match password"
+                : undefined
+            }
+          />
+          <ErrorComponent name="confirm password" />
+          <input type="submit" value="Submit" />
+        </form>
+        Renders {count}
       </div>
     </Context.Provider>
   );
