920499abd6f20d2e6b648a678f1791f9d0019240
lowefredriksson <lowe@barkh.se>
2020-11-30 15:20:28 +0100

    s

--- src/API/FieldDescription.tsx
+++ src/API/FieldDescription.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { useFormContext } from "../hooks/useFormContext";
 import { uniqueId } from "../utils/uniqueId";
 
-const useRegisterDescription = (name: string) => {
+export const useRegisterDescription = (name: string) => {
   const { addDescription, removeDescription } = useFormContext();
   const { current: id } = useRef(uniqueId(`${name}_description`));
   useEffect(() => {
@@ -12,14 +12,24 @@ const useRegisterDescription = (name: string) => {
   return id;
 };
 
+export type CustomDescriptionComponent = React.FC<{
+  id: string;
+}>;
+
 type FieldDescriptionProps = {
   name: string;
+  Component?: CustomDescriptionComponent;
 };
 
 export const FieldDescription: React.FC<FieldDescriptionProps> = ({
   name,
+  Component,
   ...rest
 }) => {
   const id = useRegisterDescription(name);
+  if (Component) {
+    return <Component id={id} {...rest} />;
+  }
+
   return <span id={id} {...rest} />;
 };
